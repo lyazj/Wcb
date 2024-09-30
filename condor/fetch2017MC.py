acbfb1ffda94024ac48118ed029300d3940e58e7
lyazj <seeson@pku.edu.cn>
2024-10-01 01:46:09 +0800

    Add fetch-supply scripts for 2017

---
+++ condor/fetch2017MC.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+from JobManager import JobManager
+
+jsons = [
+    'json/DAS_Wcb_sig_2017.json',
+    'json/DAS_Wcb_bkg_2017.json',
+]
+odirs = [
+    '/eos/user/l/legao/NtupleStore/V0/2017/MC'
+    for _ in jsons
+]
+wdirs = [
+    'production/NanoNtupleChain_11_May_2024'
+    for _ in jsons
+]
+
+name = os.path.basename(__file__)
+if name.startswith('fetch'):
+    manager = JobManager(odirs, jsons)
+    manager.fetch()
+elif name.startswith('supply'):
+    manager = JobManager(odirs, jsons, wdirs)
+    manager.supply()
+else:
+    raise ValueError('unexpected name: ' + name)
