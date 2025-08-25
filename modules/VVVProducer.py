3a7433f14dd2e2188647f93efbaae0e71319a8ea
lyazj <seeson@pku.edu.cn>
2025-08-25 22:41:38 +0800

    Update VVVProducer.py

--- modules/VVVProducer.py
+++ modules/VVVProducer.py
@@ -35,49 +35,25 @@ class VVVProducer(Module):
             self.out.branch("genWcb_mass", "F")
             self.out.branch("isWcb", "B")
 
-        self.out.branch("jetAK8puppi_pt_1", "F")
-        self.out.branch("jetAK8puppi_eta_1", "F")
-        self.out.branch("jetAK8puppi_phi_1", "F")
-        self.out.branch("jetAK8puppi_tau1_1", "F")
-        self.out.branch("jetAK8puppi_tau2_1", "F")
-        self.out.branch("jetAK8puppi_tau3_1", "F")
-        self.out.branch("jetAK8puppi_tau4_1", "F")
-        self.out.branch("jetAK8puppi_sd_1", "F")
-        self.out.branch("jetAK8puppi_sd_NoJEC_1", "F")
+        self.out.branch("nAK8Jet", "I")
+        self.out.branch("AK8Jet_pt", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_eta", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_phi", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_tau1", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_tau2", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_tau3", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_tau4", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_sdmass", "F", lenVar="nAK8Jet")
+        self.out.branch("AK8Jet_sdmass_nojec", "F", lenVar="nAK8Jet")
 
-        self.out.branch("jetAK8puppi_pt_2", "F")
-        self.out.branch("jetAK8puppi_eta_2", "F")
-        self.out.branch("jetAK8puppi_phi_2", "F")
-        self.out.branch("jetAK8puppi_tau1_2", "F")
-        self.out.branch("jetAK8puppi_tau2_2", "F")
-        self.out.branch("jetAK8puppi_tau3_2", "F")
-        self.out.branch("jetAK8puppi_tau21_2", "F")
-        self.out.branch("jetAK8puppi_tau4_2", "F")
-        self.out.branch("jetAK8puppi_tau42_2", "F")
-        self.out.branch("jetAK8puppi_sd_2", "F")
-        self.out.branch("jetAK8puppi_sd_NoJEC_2", "F")
-
-        self.out.branch("jetAK8puppi_pt_3", "F")
-        self.out.branch("jetAK8puppi_eta_3", "F")
-        self.out.branch("jetAK8puppi_phi_3", "F")
-        self.out.branch("jetAK8puppi_tau1_3", "F")
-        self.out.branch("jetAK8puppi_tau2_3", "F")
-        self.out.branch("jetAK8puppi_tau3_3", "F")
-        self.out.branch("jetAK8puppi_tau21_3", "F")
-        self.out.branch("jetAK8puppi_tau4_3", "F")
-        self.out.branch("jetAK8puppi_tau42_3", "F")
-        self.out.branch("jetAK8puppi_sd_3", "F")
-        self.out.branch("jetAK8puppi_sd_NoJEC_3", "F")
-
-        self.out.branch("Nj8", "I")
-
-        self.out.branch("ak4jet_hf", "F", lenVar="nJet")
-        self.out.branch("ak4jet_pf", "F", lenVar="nJet")
-        self.out.branch("ak4jet_pt", "F", lenVar="nJet")
-        self.out.branch("ak4jet_eta", "F", lenVar="nJet")
-        self.out.branch("ak4jet_phi", "F", lenVar="nJet")
-        self.out.branch("ak4jet_mass", "F", lenVar="nJet")
-        self.out.branch("ak4jet_deepcsvb", "F", lenVar="nJet")
+        self.out.branch("nAK4Jet", "I")
+        self.out.branch("AK4Jet_hf", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_pf", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_pt", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_eta", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_phi", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_mass", "F", lenVar="nAK4Jet")
+        self.out.branch("AK4Jet_deepcsvb", "F", lenVar="nAK4Jet")
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -109,7 +85,7 @@ class VVVProducer(Module):
             )
             nLooseElectron += passLooseElectron
             nTightElectron += passTightElectron
-            if passTightElectron:
+            if passLooseElectron:
                 self.leptons.append(TLorentzVector())
                 self.leptons[-1].SetPtEtaPhiM(
                     event.Electron_corrected_pt[iElectron],
@@ -127,18 +103,18 @@ class VVVProducer(Module):
                 event.Muon_corrected_pt[iMuon] > 20
                 and abs(muons[iMuon].eta) < 2.4
                 and muons[iMuon].Muon_looseId
-                and Muon_pfRelIso04_all < 0.25
+                and muons[iMuon].Muon_pfRelIso04_all < 0.25
             )
             passTightMuon = (
                 passLooseMuon
                 and muons[iMuon].Muon_tightId
-                and Muon_pfRelIso04_all < 0.06
+                and muons[iMuon].Muon_pfRelIso04_all < 0.06
                 and abs(muons[iMuon].Muon_dxy) < 0.05
                 and abs(muons[iMuon].Muon_dz) < 0.2
             )
             nLooseMuon += passLooseMuon
             nTightMuon += passTightMuon
-            if passTightMuon:
+            if passLooseMuon:
                 self.leptons.append(TLorentzVector())
                 self.leptons[-1].SetPtEtaPhiM(
                     event.Muon_corrected_pt[iMuon],
@@ -147,10 +123,10 @@ class VVVProducer(Module):
                     muons[iMuon].mass,
                 )
 
-        if mode == "Wcb":
+        if self.mode == "Wcb":
             if not nLooseElectron + nLooseMuon == 0:
                 return False
-        elif mode == "ttWcb":
+        elif self.mode == "ttWcb":
             if not nTightElectron + nTightMuon == 1:
                 return False
         if not Process_FatJets(self, event):
@@ -162,34 +138,10 @@ class VVVProducer(Module):
 
 def Process_GenMatching_daughterindex(event, MotherId):
     daughter_index = []
-    for id in range(0, event.nGenPart):
-        if event.GenPart_genPartIdxMother[id] == MotherId:
-            daughter_index.append(id)
+    for idx in range(0, event.nGenPart):
+        if event.GenPart_genPartIdxMother[idx] == MotherId:
+            daughter_index.append(idx)
     return daughter_index
-
-
-def Process_GenMatching_FirstCopy(event, PID):
-    FirstCopyID = -99
-    PGDID = event.GenPart_pdgId[PID]
-    LoopID = PID
-
-    while event.GenPart_pdgId[LoopID] == PGDID:
-        if (event.GenPart_statusFlags[LoopID] & (1 << 12)) > 0:
-            FirstCopyID = LoopID
-            break
-        if (
-            event.GenPart_genPartIdxMother[LoopID]
-        ) < 0:  # need to be checked more carefully
-            FirstCopyID = LoopID
-            break
-        LoopID = event.GenPart_genPartIdxMother[LoopID]
-
-    if FirstCopyID < 0:
-        LoopID = PID
-        while event.GenPart_pdgId[LoopID] == PGDID:
-            FirstCopyID = LoopID
-            LoopID = event.GenPart_genPartIdxMother[LoopID]
-    return FirstCopyID
 
 
 def Process_GenMatching_Wcb(self, event):
@@ -203,7 +155,6 @@ def Process_GenMatching_Wcb(self, event):
             W_daughter_PDG = sorted(
                 [abs(event.GenPart_pdgId[i]) for i in W_daughter_index]
             )
-            print("W_daughter_PDG:", W_daughter_PDG)
             if W_daughter_PDG == [4, 5]:
                 isWcb = True
                 pt, eta, phi, mass = (
@@ -222,15 +173,14 @@ def Process_GenMatching_Wcb(self, event):
 
 
 def Process_FatJets(self, event):
-    output, nOutput = [], 3
     fatJets = Collection(event, "FatJet")
-    iFatJets = sorted(
-        range(0, event.nFatJet), key=lambda i: fatJets[i].pt, reverse=True
-    )
-    for iFatJet in iFatJets:
-        iOutput = len(output)
-        if iOutput == nOutput:
-            break
+    pt_list, eta_list, phi_list = [], [], []
+    tau1_list, tau2_list, tau3_list, tau4_list = [], [], [], []
+    sdmass_list, sdmass_nojec_list = [], []
+
+    for iFatJet in sorted(
+        range(event.nFatJet), key=lambda i: fatJets[i].pt, reverse=True
+    ):
         fatJet = TLorentzVector()
         fatJet.SetPtEtaPhiM(
             fatJets[iFatJet].pt,
@@ -245,39 +195,33 @@ def Process_FatJets(self, event):
         if any(fatJet.DeltaR(lepton) < 0.8 for lepton in self.leptons):
             continue
 
-        self.out.fillBranch("jetAK8puppi_pt_%d" % iOutput, fatJets[iFatJet].pt)
-        self.out.fillBranch("jetAK8puppi_eta_%d" % iOutput, fatJets[iFatJet].eta)
-        self.out.fillBranch("jetAK8puppi_phi_%d" % iOutput, fatJets[iFatJet].phi)
-        self.out.fillBranch("jetAK8puppi_tau1_%d" % iOutput, fatJets[iFatJet].tau1)
-        self.out.fillBranch("jetAK8puppi_tau2_%d" % iOutput, fatJets[iFatJet].tau2)
-        self.out.fillBranch("jetAK8puppi_tau3_%d" % iOutput, fatJets[iFatJet].tau3)
-        self.out.fillBranch("jetAK8puppi_tau4_%d" % iOutput, fatJets[iFatJet].tau4)
-        self.out.fillBranch("jetAK8puppi_sd_%d" % iOutput, fatJets[iFatJet].msoftdrop)
-        self.out.fillBranch(
-            "jetAK8puppi_sd_NoJEC_%d" % iOutput, Process_FatJet_sd_NoJEC(event, iFatJet)
-        )
-        output.append(fatJet)
+        pt_list.append(fatJets[iFatJet].pt)
+        eta_list.append(fatJets[iFatJet].eta)
+        phi_list.append(fatJets[iFatJet].phi)
+        tau1_list.append(fatJets[iFatJet].tau1)
+        tau2_list.append(fatJets[iFatJet].tau2)
+        tau3_list.append(fatJets[iFatJet].tau3)
+        tau4_list.append(fatJets[iFatJet].tau4)
+        sdmass_list.append(fatJets[iFatJet].msoftdrop)
+        sdmass_nojec_list.append(Process_FatJet_sdmass_nojec(event, iFatJet))
 
-    if len(output) < 2:
-        return False
-    if all(fatJet.M() <= 30 for fatJet in output):
-        return False
+    self.out.fillBranch("nAK8Jet", len(pt_list))
+    self.out.fillBranch("AK8Jet_pt", pt_list)
+    self.out.fillBranch("AK8Jet_eta", eta_list)
+    self.out.fillBranch("AK8Jet_phi", phi_list)
+    self.out.fillBranch("AK8Jet_tau1", tau1_list)
+    self.out.fillBranch("AK8Jet_tau2", tau2_list)
+    self.out.fillBranch("AK8Jet_tau3", tau3_list)
+    self.out.fillBranch("AK8Jet_tau4", tau4_list)
+    self.out.fillBranch("AK8Jet_sdmass", sdmass_list)
+    self.out.fillBranch("AK8Jet_sdmass_nojec", sdmass_nojec_list)
 
-    self.out.fillBranch("Nj8", len(output))
-    for iOutput in range(len(output), nOutput):
-        self.out.fillBranch("jetAK8puppi_pt_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_eta_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_phi_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_tau1_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_tau2_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_tau3_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_tau4_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_sd_%d" % iOutput, -99)
-        self.out.fillBranch("jetAK8puppi_sd_NoJEC_%d" % iOutput, -99)
+    if len(pt_list) < 2 or all(sdmass <= 30 for sdmass in sdmass_list):
+        return False
     return True
 
 
-def Process_FatJet_sd_NoJEC(event, iFatJet):
+def Process_FatJet_sdmass_nojec(event, iFatJet):
     FatJet_subJetIdx1 = event.FatJet_subJetIdx1[iFatJet]
     FatJet_subJetIdx2 = event.FatJet_subJetIdx2[iFatJet]
     subjet1, subjet2, sum_p4 = TLorentzVector(), TLorentzVector(), TLorentzVector()
@@ -313,21 +257,18 @@ def Process_FatJet_sd_NoJEC(event, iFatJet):
 
 
 def Process_Jets(self, event):
-    ak4jet_hf = []
-    ak4jet_pf = []
-    ak4jet_pt = []
-    ak4jet_eta = []
-    ak4jet_phi = []
-    ak4jet_mass = []
-    ak4jet_deepcsvb = []
     jets = Collection(event, "Jet")
+    hf_list, pf_list = [], []
+    pt_list, eta_list, phi_list, mass_list = [], [], [], []
+    deepcsvb_list = []
+
     for iJet in range(len(jets)):
         jet = TLorentzVector()
         jet.SetPtEtaPhiM(
             jets[iJet].pt,
             jets[iJet].eta,
             jets[iJet].phi,
-            jets[iJet].msoftdrop,
+            jets[iJet].mass,
         )
         if jet.Pt() <= 25 or abs(jet.Eta()) >= 2.4:
             continue
@@ -335,28 +276,24 @@ def Process_Jets(self, event):
             continue
         if any(jet.DeltaR(lepton) < 0.4 for lepton in self.leptons):
             continue
-        if self.is_mc:
-            ak4jet_hf.append(jets[iJet].hadronFlavour)
-            ak4jet_pf.append(jets[iJet].partonFlavour)
-        ak4jet_pt.append(jets[iJet].pt)
-        ak4jet_eta.append(jets[iJet].eta)
-        ak4jet_phi.append(jets[iJet].phi)
-        ak4jet_mass.append(jets[iJet].mass)
-        ak4jet_deepcsvb.append(jets[iJet].btagDeepB)
-    ak4jet_hf.extend(np.zeros(event.nJet - len(ak4jet_hf), int))
-    ak4jet_pf.extend(np.zeros(event.nJet - len(ak4jet_pf), int))
-    ak4jet_pt.extend(np.zeros(event.nJet - len(ak4jet_pt), int))
-    ak4jet_eta.extend(np.zeros(event.nJet - len(ak4jet_eta), int))
-    ak4jet_phi.extend(np.zeros(event.nJet - len(ak4jet_phi), int))
-    ak4jet_mass.extend(np.zeros(event.nJet - len(ak4jet_mass), int))
-    ak4jet_deepcsvb.extend(np.zeros(event.nJet - len(ak4jet_deepcsvb), int))
-    self.out.fillBranch("ak4jet_hf", ak4jet_hf)
-    self.out.fillBranch("ak4jet_pf", ak4jet_pf)
-    self.out.fillBranch("ak4jet_pt", ak4jet_pt)
-    self.out.fillBranch("ak4jet_eta", ak4jet_eta)
-    self.out.fillBranch("ak4jet_phi", ak4jet_phi)
-    self.out.fillBranch("ak4jet_mass", ak4jet_mass)
-    self.out.fillBranch("ak4jet_deepcsvb", ak4jet_deepcsvb)
+
+        hf_list.append(jets[iJet].hadronFlavour if self.is_mc else 0)
+        pf_list.append(jets[iJet].partonFlavour if self.is_mc else 0)
+        pt_list.append(jets[iJet].pt)
+        eta_list.append(jets[iJet].eta)
+        phi_list.append(jets[iJet].phi)
+        mass_list.append(jets[iJet].mass)
+        deepcsvb_list.append(jets[iJet].btagDeepB)
+
+    self.out.fillBranch("nAK4Jet", len(hf_list))
+    self.out.fillBranch("AK4Jet_hf", hf_list)
+    self.out.fillBranch("AK4Jet_pf", pf_list)
+    self.out.fillBranch("AK4Jet_pt", pt_list)
+    self.out.fillBranch("AK4Jet_eta", eta_list)
+    self.out.fillBranch("AK4Jet_phi", phi_list)
+    self.out.fillBranch("AK4Jet_mass", mass_list)
+    self.out.fillBranch("AK4Jet_deepcsvb", deepcsvb_list)
+
     return True
 
 
