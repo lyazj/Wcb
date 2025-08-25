1759a5038d97bc00015dc59fc3beb77baabc9860
lyazj <seeson@pku.edu.cn>
2025-08-26 01:47:22 +0800

    Update

--- condor/create2016APVData.sh
+++ condor/create2016APVData.sh
@@ -2,15 +2,15 @@
 
 for ERA in B1 B2 C D E F; do
     python Condor.py --DAS DAS_2016APV${ERA}_JetHT --Filesjson "./json/DAS_2016APV${ERA}_JetHT.json" --createfilejson
-    python Condor.py --DAS DAS_2016APV${ERA}_JetHT --Filesjson "./json/DAS_2016APV${ERA}_JetHT.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/NanoNtupleChain_20_Sep_2024" --submitsh "NanoNtupleChain_20_Sep_2024.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1}'"
+    python Condor.py --DAS DAS_2016APV${ERA}_JetHT --Filesjson "./json/DAS_2016APV${ERA}_JetHT.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/Wcb/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/2016APV_Data" --submitsh "2016APV_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1} -M Wcb'"
 done
 
 for ERA in B1 B2 C D E F; do
     python Condor.py --DAS DAS_2016APV${ERA}_SingleElectron --Filesjson "./json/DAS_2016APV${ERA}_SingleElectron.json" --createfilejson
-    python Condor.py --DAS DAS_2016APV${ERA}_SingleElectron --Filesjson "./json/DAS_2016APV${ERA}_SingleElectron.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/NanoNtupleChain_20_Sep_2024" --submitsh "NanoNtupleChain_20_Sep_2024.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1}'"
+    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2016APV${ERA}_SingleElectron --Filesjson "./json/DAS_2016APV${ERA}_SingleElectron.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/${MODE}/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/2016APV_Data" --submitsh "2016APV_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1} -M ${MODE}'"; done
 done
 
 for ERA in B1 B2 C D E F; do
     python Condor.py --DAS DAS_2016APV${ERA}_SingleMuon --Filesjson "./json/DAS_2016APV${ERA}_SingleMuon.json" --createfilejson
-    python Condor.py --DAS DAS_2016APV${ERA}_SingleMuon --Filesjson "./json/DAS_2016APV${ERA}_SingleMuon.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/NanoNtupleChain_20_Sep_2024" --submitsh "NanoNtupleChain_20_Sep_2024.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1}'"
+    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2016APV${ERA}_SingleMuon --Filesjson "./json/DAS_2016APV${ERA}_SingleMuon.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/${MODE}/Data" --year 2016APV --excutable "exe_CUSTNANO_UL16_MINIAODv2_DATA.sh" --TaskFolder "production/2016APV_Data" --submitsh "2016APV_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2016_preVFP${ERA:0:1} -M ${MODE}'"; done
 done
--- condor/create2016APVMC.sh
+++ condor/create2016APVMC.sh
@@ -4,6 +4,8 @@ python Condor.py --DAS DAS_Wcb_sig_2016APV --Filesjson "./json/DAS_Wcb_sig_2016A
 python Condor.py --DAS DAS_Wcb_bkg_2016APV --Filesjson "./json/DAS_Wcb_bkg_2016APV.json" --createfilejson &
 wait
 
-python Condor.py --DAS DAS_Wcb_sig_2016APV --Filesjson "./json/DAS_Wcb_sig_2016APV.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/MC" --year 2016APV --excutable "exe_UL16_Wcb_NanoNtupleChain.sh" --TaskFolder "production/NanoNtupleChain_21_Sep_2024" --submitsh "NanoNtupleChain_21_Sep_2024.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2016pre -M Wcb'" &
-python Condor.py --DAS DAS_Wcb_bkg_2016APV --Filesjson "./json/DAS_Wcb_bkg_2016APV.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2016APV/MC" --year 2016APV --excutable "exe_UL16_Wcb_NanoNtupleChain.sh" --TaskFolder "production/NanoNtupleChain_21_Sep_2024" --submitsh "NanoNtupleChain_21_Sep_2024.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2016pre'" &
+for MODE in Wcb ttWcb; do
+    python Condor.py --DAS DAS_Wcb_sig_2016APV --Filesjson "./json/DAS_Wcb_sig_2016APV.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/${MODE}/2016APV/MC" --year 2016APV --excutable "exe_UL16_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2016APV_MC" --submitsh "2016APV_MC.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2016pre -M ${MODE}'" &
+    python Condor.py --DAS DAS_Wcb_bkg_2016APV --Filesjson "./json/DAS_Wcb_bkg_2016APV.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/${MODE}/2016APV/MC" --year 2016APV --excutable "exe_UL16_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2016APV_MC" --submitsh "2016APV_MC.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2016pre -M ${MODE}'" &
+done
 wait
