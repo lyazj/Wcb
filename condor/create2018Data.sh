4775b2779a0a98dfe29c458797ce89a76cf2bd8b
lyazj <seeson@pku.edu.cn>
2025-08-25 20:36:45 +0200

    Update

--- condor/create2018Data.sh
+++ condor/create2018Data.sh
@@ -2,15 +2,15 @@
 
 for ERA in A B C D; do
     python Condor.py --DAS DAS_2018${ERA}_JetHT --Filesjson "./json/DAS_2018${ERA}_JetHT.json" --createfilejson
-    python Condor.py --DAS DAS_2018${ERA}_JetHT --Filesjson "./json/DAS_2018${ERA}_JetHT.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data" --submitsh "2018_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M Wcb'"
+    python Condor.py --DAS DAS_2018${ERA}_JetHT --Filesjson "./json/DAS_2018${ERA}_JetHT.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data_Wcb" --submitsh "2018_Data_Wcb.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M Wcb'"
 done
 
 for ERA in A B C D; do
     python Condor.py --DAS DAS_2018${ERA}_EGamma --Filesjson "./json/DAS_2018${ERA}_EGamma.json" --createfilejson
-    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2018${ERA}_EGamma --Filesjson "./json/DAS_2018${ERA}_EGamma.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data" --submitsh "2018_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M ${MODE}'"; done
+    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2018${ERA}_EGamma --Filesjson "./json/DAS_2018${ERA}_EGamma.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data_${MODE}" --submitsh "2018_Data_${MODE}.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M ${MODE}'"; done
 done
 
 for ERA in A B C D; do
     python Condor.py --DAS DAS_2018${ERA}_SingleMuon --Filesjson "./json/DAS_2018${ERA}_SingleMuon.json" --createfilejson
-    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2018${ERA}_SingleMuon --Filesjson "./json/DAS_2018${ERA}_SingleMuon.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data" --submitsh "2018_Data.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M ${MODE}'"; done
+    for MODE in Wcb ttWcb; do python Condor.py --DAS DAS_2018${ERA}_SingleMuon --Filesjson "./json/DAS_2018${ERA}_SingleMuon.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/Data" --year 2018 --excutable "exe_CUSTNANO_UL18_MINIAODv2_DATA.sh" --TaskFolder "production/2018_Data_${MODE}" --submitsh "2018_Data_${MODE}.sh" --Condor --AddtionalArgs "-a '-o ./ -d --year UL2018${ERA} -M ${MODE}'"; done
 done
--- condor/create2018MC.sh
+++ condor/create2018MC.sh
@@ -5,7 +5,7 @@ python Condor.py --DAS DAS_Wcb_bkg_2018 --Filesjson "./json/DAS_Wcb_bkg_2018.jso
 wait
 
 for MODE in Wcb ttWcb; do
-    python Condor.py --DAS DAS_Wcb_sig_2018 --Filesjson "./json/DAS_Wcb_sig_2018.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/${MODE}/2018/MC" --year 2018 --excutable "exe_UL18_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2018_MC" --submitsh "2018_MC.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2018 -M ${MODE}'" &
-    python Condor.py --DAS DAS_Wcb_bkg_2018 --Filesjson "./json/DAS_Wcb_bkg_2018.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/${MODE}/2018/MC" --year 2018 --excutable "exe_UL18_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2018_MC" --submitsh "2018_MC.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2018 -M ${MODE}'" &
+    python Condor.py --DAS DAS_Wcb_sig_2018 --Filesjson "./json/DAS_Wcb_sig_2018.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/MC" --year 2018 --excutable "exe_UL18_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2018_MC_${MODE}" --submitsh "2018_MC_${MODE}.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2018 -M ${MODE}'" &
+    python Condor.py --DAS DAS_Wcb_bkg_2018 --Filesjson "./json/DAS_Wcb_bkg_2018.json" --outputPath "/eos/user/l/legao/NtupleStore/V0/2018/${MODE}/MC" --year 2018 --excutable "exe_UL18_Wcb_NanoNtupleChain.sh" --TaskFolder "production/2018_MC_${MODE}" --submitsh "2018_MC_${MODE}.sh" --Condor --AddtionalArgs "-a '-o ./ -m --year 2018 -M ${MODE}'" &
 done
 wait
