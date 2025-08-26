53784bee6c6d0560f4ab3d0b1eb48a372f811bcd
lyazj <seeson@pku.edu.cn>
2025-08-26 05:22:57 +0200

    Add 2018

---
+++ condor/2018_Data_ttWcb.sh
@@ -0,0 +1,8 @@
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/EGamma_Run2018A-UL2018_MiniAODv2-v1/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/EGamma_Run2018B-UL2018_MiniAODv2-v1/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/EGamma_Run2018C-UL2018_MiniAODv2-v1/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/EGamma_Run2018D-UL2018_MiniAODv2-v2/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/SingleMuon_Run2018A-UL2018_MiniAODv2-v3/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/SingleMuon_Run2018B-UL2018_MiniAODv2-v2/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/SingleMuon_Run2018C-UL2018_MiniAODv2-v2/submit.cmd 
+condor_submit /afs/cern.ch/user/l/legao/Wcb/condor/production/2018_Data_ttWcb/SingleMuon_Run2018D-UL2018_MiniAODv2-v3/submit.cmd 
