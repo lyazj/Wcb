a10eb0de9c082244e3ec420731fca4bd06f3e270
lyazj <seeson@pku.edu.cn>
2025-08-30 14:46:55 +0800

    Update fetch2018Data.py

--- condor/fetch2018Data.py
+++ condor/fetch2018Data.py
@@ -4,22 +4,70 @@ import os
 from JobManager import JobManager
 
 jsons = [
-    #'json/DAS_2018A_JetHT.json',
-    #'json/DAS_2018B_JetHT.json',
-    #'json/DAS_2018C_JetHT.json',
-    #'json/DAS_2018D_JetHT.json',
+    'json/DAS_2018A_JetHT.json',
+    'json/DAS_2018B_JetHT.json',
+    'json/DAS_2018C_JetHT.json',
+    'json/DAS_2018D_JetHT.json',
+    'json/DAS_2018A_EGamma.json',
+    'json/DAS_2018B_EGamma.json',
+    'json/DAS_2018C_EGamma.json',
+    'json/DAS_2018D_EGamma.json',
+    'json/DAS_2018A_SingleMuon.json',
+    'json/DAS_2018B_SingleMuon.json',
+    'json/DAS_2018C_SingleMuon.json',
+    'json/DAS_2018D_SingleMuon.json',
+    'json/DAS_2018A_EGamma.json',
+    'json/DAS_2018B_EGamma.json',
+    'json/DAS_2018C_EGamma.json',
+    'json/DAS_2018D_EGamma.json',
     'json/DAS_2018A_SingleMuon.json',
     'json/DAS_2018B_SingleMuon.json',
     'json/DAS_2018C_SingleMuon.json',
     'json/DAS_2018D_SingleMuon.json',
 ]
 odirs = [
-    '/eos/user/l/legao/NtupleStore/V0/2018/Data'
-    for _ in jsons
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/Wcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
+    '/eos/user/l/legao/NtupleStore/V0/2018/ttWcb/Data',
 ]
 wdirs = [
-    'production/NanoNtupleChain_25_Apr_2024'
-    for _ in jsons
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_Wcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
+    'production/2018_Data_ttWcb',
 ]
 
 name = os.path.basename(__file__)
