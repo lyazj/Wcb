26dbdc95ecb5db451184adc47648c45d621befa0
lyazj <seeson@pku.edu.cn>
2025-09-02 04:14:01 +0200

    Avoid writing empty files

--- condor/exec/exe_UL17_Wcb_NanoNtupleChain.sh
+++ condor/exec/exe_UL17_Wcb_NanoNtupleChain.sh
@@ -221,10 +221,19 @@ EOL
 #    fi
 #    exit 0  # We didn't succeed. But we don't want to try again.
 #fi
-if [ "${OFILE:0:4}" = "/eos" ]; then
-    xrdcp tree.root root://eosuser.cern.ch/${OFILE}
+if [ "$(python2 /afs/cern.ch/user/l/legao/.local/bin/count-events tree.root | egrep -o '[0-9]+$')" = 0 ]; then
+    : > tree.root.out
+    if [ "${OFILE:0:4}" = "/eos" ]; then
+        xrdcp tree.root.out root://eosuser.cern.ch/${OFILE}.out
+    else
+        rsync tree.root.out ${OFILE}.out
+    fi
 else
-    rsync tree.root ${OFILE}
+    if [ "${OFILE:0:4}" = "/eos" ]; then
+        xrdcp tree.root root://eosuser.cern.ch/${OFILE}
+    else
+        rsync tree.root ${OFILE}
+    fi
 fi
 
 cd $BASEPATH
