d7c5ff6446a50997be02e05d42aa3b096b675332
lyazj <seeson@pku.edu.cn>
2025-09-02 21:48:58 +0800

    Avoid removing empty MC file

--- condor/JobManager.py
+++ condor/JobManager.py
@@ -111,7 +111,8 @@ class JobManager:
         delete_args = ['ssh', 'lxplus.cern.ch', 'rm'] + [os.path.join(odir, 'out_%d.root') % job for job in good_jobs]
         print('Generating %s' % object_name)
         os.sys.stdout.flush()
-        self.applications.append(self.pool.apply_async(run_and, [clean_args, setup_args, fetch_args, count_args, merge_args, verify_args, rename_args, clean_args, markup_args, delete_args]))
+        #self.applications.append(self.pool.apply_async(run_and, [clean_args, setup_args, fetch_args, count_args, merge_args, verify_args, rename_args, clean_args, markup_args, delete_args]))
+        self.applications.append(self.pool.apply_async(run_and, [clean_args, setup_args, fetch_args, merge_args, verify_args, rename_args, clean_args, markup_args, delete_args]))
 
     def wait_fetch_jobs(self):
         while self.applications:
