75e0fdedd932e8f67469019c26232f3b4df0abe0
lyazj <seeson@pku.edu.cn>
2025-09-08 00:48:19 +0800

    Fix memory issue and enable para for plot_parquet.py

--- new/plot_parquet.py
+++ new/plot_parquet.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mplhep as hep
 import natsort
+import multiprocessing
 
 plt.figure(figsize=(15, 12))
 hep.style.use("CMS")
@@ -18,13 +19,13 @@ def cms_label(year):
 
 
 year_match = {
-    # "2016APV": ["2016APV"],
-    # "2016": ["2016"],
-    # "2017": ["2017"],
+    #"2016APV": ["2016APV"],
+    #"2016": ["2016"],
+    #"2017": ["2017"],
     "2018": ["2018"],
 }
 modes = [
-    # "Wcb",
+    "Wcb",
     "ttWcb",
 ]
 proc_match = {
@@ -88,24 +89,54 @@ proc_color = {
     "Other": list(plt.rcParams["axes.prop_cycle"])[5]["color"],
 }
 proc_cut = {
-    "all": ["AK8Jet_pt[..., 0] > 350"],
-    "mode:Wcb": ["~exclusive_btag_tight"],
-    "mode:ttWcb": ["exclusive_btag_tight"],
-    "Wcb": ["isWcb"],
-    "WJets": ["~isWcb"],
+    "all": [lambda ev: ev["AK8Jet_pt"][..., 0] > 350],
+    "mode:Wcb": [lambda ev: ~ak.any(ev["AK4Jet_exclusive"] & ev["AK4Jet_btag_tight"], axis=-1)],
+    "mode:ttWcb": [lambda ev: ak.any(ev["AK4Jet_exclusive"] & ev["AK4Jet_btag_tight"], axis=-1)],
+    "Wcb": [lambda ev: ev["isWcb"]],
+    "WJets": [lambda ev: ~ev["isWcb"]],
 }
 plot_list = [
-    ("ak8_1_pt", r"leading AK8 jet $p_\mathrm{T}$ [GeV]", "AK8Jet_pt[..., 0]", 350, 850, 10),
-    ("ak8_1_eta", r"leading AK8 jet $\eta$", "AK8Jet_eta[..., 0]", -2.5, 2.5, 0.2),
-    ("ak8_1_phi", r"leading AK8 jet $\phi$", "AK8Jet_phi[..., 0]", -np.pi, np.pi, 0.2 * np.pi),
-    ("ak8_1_sdmass", r"leading AK8 jet $m_\mathrm{SD}$ [GeV]", "AK8Jet_sdmass[..., 0]", 30, 230, 10),
+    ("ak8_1_pt", r"leading AK8 jet $p_\mathrm{T}$ [GeV]", lambda ev: ev["AK8Jet_pt"][..., 0], 350, 850, 10),
+    ("ak8_1_eta", r"leading AK8 jet $\eta$", lambda ev: ev["AK8Jet_eta"][..., 0], -2.5, 2.5, 0.2),
+    ("ak8_1_phi", r"leading AK8 jet $\phi$", lambda ev: ev["AK8Jet_phi"][..., 0], -np.pi, np.pi, 0.2 * np.pi),
+    ("ak8_1_sdmass", r"leading AK8 jet $m_\mathrm{SD}$ [GeV]", lambda ev: ev["AK8Jet_sdmass"][..., 0], 30, 230, 10),
 ]
 blind_match = {
-    # "ak8_1_sdmass": [(50, 110)],
+    #"ak8_1_sdmass": [(50, 110)],
 }
 indir = "/data/bond/lyazj/Parquet/V0"
 outdir = "parquet"
 os.makedirs(outdir, exist_ok=True)
+
+
+def parquet_to_hists(mode, proc, fpath):
+    events = ak.from_parquet(fpath)
+    nevent_precut = len(events)
+    for cut in proc_cut.get("all", []) + proc_cut.get("mode:" + mode, []) + proc_cut.get(proc, []):
+        events = events[cut(events)]
+    nevent_postcut = len(events)
+    print(f"{fpath}: {nevent_precut} -> {nevent_postcut}")
+    hists = []
+    for n, l, ex, b, e, s in plot_list:
+        bev = events
+        bevex = ex(bev)
+        if proc == "Data":
+            for bl, br in blind_match.get(n, []):
+                mask = (bevex < bl) | (bevex > br)
+                bev = bev[mask]
+                bevex = bevex[mask]
+        weights = bev["weight"]
+        hists.append(np.histogram(bevex.to_numpy(), bins=np.arange(b, e + s, s), weights=weights.to_numpy())[0])
+    return np.array(hists, dtype=object)
+
+
+def parquet_to_hists_async(mode, proc, fpath):
+    queue = multiprocessing.Queue()
+    proc = multiprocessing.Process(target=lambda: queue.put(parquet_to_hists(mode, proc, fpath)))
+    proc.daemon = True  # Not equivalent but similar to pthread_detach().
+    proc.start()
+    return queue
+
 
 hists = {}
 for year in year_match:
@@ -114,39 +145,20 @@ for year in year_match:
         hists[year][mode] = {}
         for proc in proc_match:
             print(proc)
-            hists[year][mode][proc] = [np.zeros(int(round((e - b) / s))) for (n, l, ex, b, e, s) in plot_list]
+            hists[year][mode][proc] = []
             for ym in year_match[year]:
                 dirname = os.path.join(indir, ym, mode, "Data" if proc == "Data" else "MC")
                 for pm in natsort.natsorted(os.listdir(dirname)) if proc == "Data" else proc_match[proc]:
                     if not pm.endswith(".parquet"):
                         continue
+                    if mode == "Wcb" and proc == "Data" and "JetHT" not in pm:
+                        continue
                     if mode == "Wcb" and proc == "Wcb" and "TT" in pm:
                         continue
                     if mode == "ttWcb" and proc == "Wcb" and "WJets" in pm:
                         continue
-                    fpath = os.path.join(dirname, pm)
-                    events = ak.from_parquet(fpath)
-                    events["exclusive_btag_tight"] = ak.any(
-                        events["AK4Jet_exclusive"] & events["AK4Jet_btag_tight"], axis=1
-                    )
-                    nevent_precut = len(events)
-                    for cut in proc_cut.get("all", []) + proc_cut.get("mode:" + mode, []) + proc_cut.get(proc, []):
-                        events = events[eval(cut, None, events)]
-                    nevent_postcut = len(events)
-                    print(f"{fpath}: {nevent_precut} -> {nevent_postcut}")
-                    for i, (n, l, ex, b, e, s) in enumerate(plot_list):
-                        bev = events
-                        bevex = eval(ex, None, bev)
-                        if proc == "Data":
-                            for bl, br in blind_match.get(n, []):
-                                mask = (bevex < bl) | (bevex > br)
-                                bev = bev[mask]
-                                bevex = bevex[mask]
-                        weights = bev["weight"]
-                        hist, _ = np.histogram(
-                            bevex.to_numpy(), bins=np.arange(b, e + s, s), weights=weights.to_numpy()
-                        )
-                        hists[year][mode][proc][i] += hist
+                    hists[year][mode][proc].append(parquet_to_hists_async(mode, proc, os.path.join(dirname, pm)))
+            hists[year][mode][proc] = sum(hist.get() for hist in hists[year][mode][proc])
 hists["all"] = {}
 for mode in modes:
     hists["all"][mode] = {}
@@ -156,8 +168,8 @@ for mode in modes:
             for i, (n, l, ex, b, e, s) in enumerate(plot_list):
                 hists["all"][mode][proc][i] += hists[year][mode][proc][i]
 
-# for year in list(year_match) + ["all"]:
-for year in list(year_match):
+#for year in year_match + ["all"]:
+for year in year_match:
     for mode in modes:
         for i, (n, l, ex, b, e, s) in enumerate(plot_list):
             print(year, mode, n)
@@ -170,8 +182,11 @@ for year in list(year_match):
                 cs.append(proc_color[proc])
             hep.histplot(hs[0], histtype="errorbar", label=ls[0], color=cs[0])
             hep.histplot(hs[1:-1], stack=True, histtype="fill", label=ls[1:-1], color=cs[1:-1])
-            hep.histplot((hs[-1][0] * 1000000, hs[-1][1]), histtype="step", label=ls[-1] + " (×1000000)", color=cs[-1])
-            cms_label(year)
+            sf = np.sum(hs[0][0]) / np.sum(hs[-1][0])
+            sf_down = 10 ** int(np.log10(sf))
+            sf = sf_down * 10 if sf >= sf_down * 7.5 else sf_down * 5 if sf >= sf_down * 2.5 else sf_down
+            hep.histplot((hs[-1][0] * sf, hs[-1][1]), histtype="step", label=ls[-1] + f" (×{sf})", color=cs[-1])
+            cms_label(year + " " + mode)
             plt.legend(loc="upper left", ncols=3)
             plt.grid()
             plt.xlabel(l)
