afb94636f6ca27165c08f8722923d4c8c4f4a146
lyazj <seeson@pku.edu.cn>
2025-10-28 17:27:24 +0800

    Move Wcb candidate -> leading AK8 jet for HLT correction

--- new/make_parquet_trigeff.py
+++ new/make_parquet_trigeff.py
@@ -61,8 +61,8 @@ proc_cut = {
     "all": [],
 }
 plots = [
-    ("ak8_1_pt", r"$W \to cb$ candidate jet $p_\mathrm{T}$ [GeV]", lambda ev: ev["AK8Jet_pt"][..., 0], 200, 600, 10),
-    ("ak8_1_sdmass", r"$W \to cb$ candidate jet $m_\mathrm{SD}$ [GeV]", lambda ev: ev["AK8Jet_sdmass"][..., 0], 30, 230, 10),
+    ("ak8_a_pt", r"leading AK8 jet $p_\mathrm{T}$ [GeV]", lambda ev: ev["AK8Jet_pt"][ak.argmax(ev["AK8Jet_pt"], axis=-1)[:, None]][..., 0], 200, 600, 10),
+    ("ak8_a_sdmass", r"leading AK8 jet $m_\mathrm{SD}$ [GeV]", lambda ev: ev["AK8Jet_sdmass"][ak.argmax(ev["AK8Jet_pt"], axis=-1)[:, None]][..., 0], 30, 230, 10),
 ]
 indir = "/data/bond/lyazj/Parquet/V0"
 outdir = "parquet"
