2933fbd62a30fbcf112c56d0a52997773a3e594c
lyazj <seeson@pku.edu.cn>
2025-11-10 23:50:45 +0800

    Update scripts

--- new/run_parquet.sh
+++ new/run_parquet.sh
@@ -1,8 +1,8 @@
-#./run_parquet.py --version V0 --year 2018 --mode Wcb --type Data
-#./run_parquet.py --version V0 --year 2018 --mode Wcb --type MC
-#./run_parquet.py --version V0 --year 2018 --mode ttWcb --type Data
-#./run_parquet.py --version V0 --year 2018 --mode ttWcb --type MC
-#./run_parquet.py --version V0 --year 2017 --mode Wcb --type Data
-#./run_parquet.py --version V0 --year 2017 --mode Wcb --type MC
-#./run_parquet.py --version V0 --year 2017 --mode ttWcb --type Data
-#./run_parquet.py --version V0 --year 2017 --mode ttWcb --type MC
+./run_parquet.py --version V0 --year 2018 --mode Wcb --type Data
+./run_parquet.py --version V0 --year 2018 --mode Wcb --type MC
+./run_parquet.py --version V0 --year 2018 --mode ttWcb --type Data
+./run_parquet.py --version V0 --year 2018 --mode ttWcb --type MC
+./run_parquet.py --version V0 --year 2017 --mode Wcb --type Data
+./run_parquet.py --version V0 --year 2017 --mode Wcb --type MC
+./run_parquet.py --version V0 --year 2017 --mode ttWcb --type Data
+./run_parquet.py --version V0 --year 2017 --mode ttWcb --type MC
