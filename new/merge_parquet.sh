32f7c37fdca6d1af5da884a231ad9892b59abe8e
lyazj <seeson@pku.edu.cn>
2025-11-10 23:13:43 +0800

    Add merge_parquet.sh

---
+++ new/merge_parquet.sh
@@ -0,0 +1,8 @@
+#./merge_parquet.py --version V0 --year 2018 --mode Wcb --type Data
+#./merge_parquet.py --version V0 --year 2018 --mode Wcb --type MC
+#./merge_parquet.py --version V0 --year 2018 --mode ttWcb --type Data
+#./merge_parquet.py --version V0 --year 2018 --mode ttWcb --type MC
+#./merge_parquet.py --version V0 --year 2017 --mode Wcb --type Data
+#./merge_parquet.py --version V0 --year 2017 --mode Wcb --type MC
+#./merge_parquet.py --version V0 --year 2017 --mode ttWcb --type Data
+#./merge_parquet.py --version V0 --year 2017 --mode ttWcb --type MC
